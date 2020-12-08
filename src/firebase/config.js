94bc7118de54ba675155bf5c39aaefd2c0d652a7
Nikhilthehokage <51057230+Nikhilthehokage@users.noreply.github.com>
2020-12-08 17:46:08 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,14 +5,8 @@ import 'firebase/firestore';
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyC9f8iFHaCIdrmUSufcMAAtxQPN5sts3a0",
-    authDomain: "smarteats-36190.firebaseapp.com",
-    databaseURL: "https://smarteats-36190.firebaseio.com",
-    projectId: "smarteats-36190",
-    storageBucket: "smarteats-36190.appspot.com",
-    messagingSenderId: "246065007931",
-    appId: "1:246065007931:web:8daf080cb3822622a43b1e",
-    measurementId: "G-TLWDS27FD9"
+    // paste your firebase config
+    
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
